8db3dda7ffbadb1da1b1eeba7fbd2cfc013b2c31
Yaroslav Rogoza <rogyar@gmail.com>
2015-03-15 10:29:50 +0200

    Mark product as disabled and archived on delete

--- app/code/Atwix/ArchiveProduct/Controller/Adminhtml/Product/MassDelete.php
+++ app/code/Atwix/ArchiveProduct/Controller/Adminhtml/Product/MassDelete.php
@@ -1,9 +1,5 @@
 <?php
-/**
- *
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
- */
+
 namespace Atwix\ArchiveProduct\Controller\Adminhtml\Product;
 
 class MassDelete extends \Magento\Catalog\Controller\Adminhtml\Product\MassDelete
@@ -19,11 +15,16 @@ class MassDelete extends \Magento\Catalog\Controller\Adminhtml\Product\MassDelet
         } else {
             try {
                 foreach ($productIds as $productId) {
+                    // TODO: Might be shared as a lib method
+                    // TODO: Check associated products and mark them as disabled as well
+
+                    /** @var \Magento\Catalog\Model\Product $product */
                     $product = $this->_objectManager->get('Magento\Catalog\Model\Product')->load($productId);
-                    $product->delete();
+                    $product->setStatus(2)->setIsArchived(1);
+                    $product->save();
                 }
                 $this->messageManager->addSuccess(
-                    __('A total of %1 record(s) have been deleted.', count($productIds))
+                    __('A total of %1 record(s) have been moved to trash.', count($productIds))
                 );
             } catch (\Exception $e) {
                 $this->messageManager->addError($e->getMessage());
